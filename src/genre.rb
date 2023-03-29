d90afe4ea053b4e2f95fb785c9c32f0c2c9fb776
Abel Seifu <abel.workmail@gmail.com>
2023-03-29 14:41:28 +0300

    Add genre class

---
+++ src/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+	attr_reader :name, :items
+
+	def initialize(name)
+		@id = Random.new.rand(1..1000)
+		@name = name
+		@items = []
+	end
+
+	def add_item(item)
+		@items.push(item)
+		item.genre = self
+	end
+end
